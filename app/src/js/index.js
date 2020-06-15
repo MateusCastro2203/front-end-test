f4786cd28815c48671d4bca956fca6b7f03b97d9
MateusCastro2203 <mateustcastro@gmail.com>
2020-06-15 17:24:09 -0300

    finish

--- app/src/js/index.js
+++ app/src/js/index.js
@@ -1,74 +1,96 @@
-import 'bootstrap';
-import '../scss/style.scss';
-jQuery.support.cors = true;
+(()=>{
+if (typeof require === 'function'){
+bootstrap = require ('bootstrap');
+style = require('../scss/style.scss');
+}
+}
+)();
 
-let resquestUrl = 'https://raw.githubusercontent.com/MateusCastro2203/front-end-test/master/sample-data.json';
-let request = new XMLHttpRequest();
-let div = document.querySelector('section');
+window.div = document.getElementById('events');
+let dataSet = [];
 
-request.open('GET', resquestUrl);
-request.responseType = 'json';
-request.send();
 
-request.onload = function(){
-    let events = request.response;
-    showEvents(events);
+
+function getRequest(){
+    let resquestUrl = 'https://raw.githubusercontent.com/MateusCastro2203/front-end-test/master/sample-data.json';
+    let request = new XMLHttpRequest();
+
+    request.open('GET', resquestUrl);
+    request.responseType = 'json';
+    request.send();
+    request.onload = function(){
+        let events = request.response;
+        showEvents(events);
+    }
 }
 
 function showEvents(jsonObj){
-    let events = jsonObj['events'];
-    
-
-    for(let i = 0; i < events.length; i++){
-        let card = document.createElement('article');
-        let title = document.createElement('h2');
-        let button = document.getElementById("button");
-        let description = document.createElement('p');
-        let img = document.createElement('img');
-        let start = document.createElement('p');
-        let end = document.createElement('p');
-        let recurrence = document.createElement('p');
-        let costs = document.createElement('p');
-        let link = document.createElement('a');
-        let venueName = document.createElement('p');
-        let venueZip = document.createElement('p');
-        let venueCity = document.createElement('p');
-        let venueStreet = document.createElement('p');
-        let category = document.createElement('p');
-
-        title.textContent = events[i].title;
-        img.src='../src/assets/img/'+ events[i].image;
-        description.textContent = events[i].description;
-        start.textContent = events[i].start;
-        end.textContent = events[i].end;
-        recurrence.textContent = 'Recurrence: ' + events[i].recurrence;
-        costs.textContent = 'Price: '+ events[i].costs;
-        link.src = events[i].link;
-        venueName.textContent ='Venue: ' + events[i].venue.name;
-        venueZip.textContent = events[i].venue.zip;
-        venueCity.textContent = 'City: '+ events[i].venue.city;
-        venueStreet.textContent = 'Street: ' + events[i].venue.street;
-        category.textContent = 'Category: '+ events[i].category;
-
-        card.appendChild(title);
-        events[i].image ? card.appendChild(img) : img;
-        card.appendChild(description);
-        card.appendChild(start);
-        card.appendChild(end);
-        card.appendChild(recurrence);
-        card.appendChild(costs);
-        card.appendChild(link);
-        card.appendChild(venueName);
-        card.appendChild(venueCity);
-        card.appendChild(venueStreet);
-        card.appendChild(venueZip);
-        
-        card.appendChild(category);
-
-        div.appendChild(card);
-
-        console.log(img);
-    }
-    
+    dataSet = jsonObj['events'];
+    dataSet.sort((a,b)=>{return a.start-b.start})
+    console.log(events);
+    createCard();
 }
- 
+
+function createCard(){
+    dataSet.forEach( events => {
+        let templateCard = `
+            <div class='container card'>
+                <h3 class = "title"> ${events.title}</h3>
+                ${events.image ? "<img src='../src/assets/img/"+ events.image+"'>" : ''}
+                <p class='description'>${events.description}</p>
+                <a class="btn btn-outline-dark" data-toggle="collapse" href="#collapse">
+                    More
+                </a>
+                <div class="collapse values" id="collapse">
+                    <p>Day: ${setDate(events.start)}</p>
+                    <p>Hour: ${setTime(events.start)}</p>
+                    <p>Finish: ${setDate(events.end)} <br> Hour: ${setTime(events.end)}</p>
+                    <p>Price: ${events.costs} </p>
+                    <p>Recurrence: ${events.recurrence}</p>
+                    <a href="${events.link}" target="blanc" >Event link</a> <br>
+                    <a class="btn btn-outline-dark" data-toggle="collapse" href="#Venue">
+                     Vanue
+                    </a>
+                    <div class="collapse values" id="Venue">
+                        <p>${events.venue.name}</p>
+                        <p>City: ${events.venue.city}</p>
+                        <p>Street: ${events.venue.street}</p>
+                        <p>Zip: ${events.venue.zip}</p>
+                    </div>
+
+                </div>
+                <br>
+            </div>
+        `;
+        let cardHTML = new DOMParser().parseFromString(templateCard, 'text/html');
+        cardHTML = cardHTML.body.childNodes[0];
+        document.getElementById('events').appendChild(cardHTML);
+    })
+}
+
+function setDate(dateStart){
+    let date = new Date (dateStart*1000);
+    let day = date.toLocaleDateString();
+    return day;
+}
+
+function setTime(dateStart){
+    let date = new Date (dateStart*1000);
+    let time = date.toLocaleTimeString();
+    return time;
+}
+
+this.datePicker = function() {
+    let date = document.getElementById('datetimepicker1').value;
+    let value = new Date(date).getTime() / 1000;
+    Array.from(div.getElementsByClassName("card")).forEach((event,index) =>{
+        if(dataSet[index].start >= value){
+            event.classList.remove('hidde');
+        }else if (!event.classList.contains('hidde')) {
+            event.classList.add('hidde');
+        }
+    })
+    
+};
+
+
